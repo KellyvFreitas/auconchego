53f58b51b9bff278f4435dd8cc305d21bb176231
Kelly Freitas <kellyvfreitas@live.com>
2025-02-03 09:14:03 -0300

    AJUSTE BOTÃO CONTATOS

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,23 +1,47 @@
 <template>
-    <q-card-section class="footer-container flex-center flex q-pa-none">
-      <img class="image q-mb-lg" style="max-width: 200px;" src="/src/assets/image.png">
-      <div  class="flex-center q-mb-md ">
-        <router-link to="/#">
-          <q-btn class="buttons" flat label="Página Inicial"></q-btn>
-        </router-link>
-        <router-link to="/quero-adotar">
-          <q-btn class="buttons" flat label="Quero Adotar"></q-btn>
-        </router-link>
-        <router-link to="/quero-ajudar">
-          <q-btn class="buttons" flat label="Quero Ajudar"></q-btn>
-        </router-link>
-        <router-link to="/voluntarios">
-          <q-btn class="buttons" flat label="Auconchego"></q-btn>
-        </router-link>
-        <q-btn unelevated rounded style="background-color: #BBED2F; width: 20vh; margin-left: 40px" label="Contatos"></q-btn>
-      </div>
-    </q-card-section>
+  <q-card-section class="footer-container flex-center flex q-pa-none">
+    <img class="image q-mb-lg" style="max-width: 200px;" src="/src/assets/image.png">
+    <div class="flex-center q-mb-md">
+      <router-link to="/#">
+        <q-btn class="buttons" flat label="Página Inicial"></q-btn>
+      </router-link>
+      <router-link to="/quero-adotar">
+        <q-btn class="buttons" flat label="Quero Adotar"></q-btn>
+      </router-link>
+      <router-link to="/quero-ajudar">
+        <q-btn class="buttons" flat label="Quero Ajudar"></q-btn>
+      </router-link>
+      <router-link to="/voluntarios">
+        <q-btn class="buttons" flat label="Auconchego"></q-btn>
+      </router-link>
+      <q-btn
+        unelevated
+        rounded
+        style="background-color: #BBED2F; width: 20vh; margin-left: 40px"
+        label="Contatos"
+        @click="scrollToFooter"
+      />
+    </div>
+  </q-card-section>
 </template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  setup() {
+    const scrollToFooter = () => {
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth",
+      });
+    };
+
+    return {
+      scrollToFooter,
+    };
+  },
+};
+</script>
 
 <style scoped>
 .buttons {
@@ -30,9 +54,8 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  clip-path: path('M0,0 L0,150 Q600,250 1200,200 T2400,200 L2400,0 Z');
+  clip-path: path("M0,0 L0,150 Q600,250 1200,200 T2400,200 L2400,0 Z");
   box-sizing: border-box;
-  background: linear-gradient(100deg, #5A41A0, #3A2A7A);
+  background: linear-gradient(100deg, #5a41a0, #3a2a7a);
 }
 </style>
-
